0586d199911a946d9965986256b27284f4d76612
GerardoGM14 <gerardocampeon1@hotmail.com>
2025-06-02 14:58:13 -0500

    v2

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,12 +1,8 @@
 <!-- resources/views/login.blade.php -->
 <!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <title>Login</title>
-</head>
+<html>
+<head><title>Login</title></head>
 <body>
-    <h1>Bienvenido a AutoClean360</h1>
-    <p>Esta es la vista de login funcionando desde Render.</p>
+  <h1>Pantalla de Login</h1>
 </body>
 </html>
