0aa7ab8419c74a5c72f5c89412156358d44d3414
GerardoGM14 <163801892+GerardoGM14@users.noreply.github.com>
2025-06-11 15:02:38 -0500

    Update firebase.js

--- resources/js/firebase.js
+++ resources/js/firebase.js
@@ -9,13 +9,13 @@ import { getMessaging } from "firebase/messaging";
 
 // Configuración de tu Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyAykf_rLw3KLeHiGi_UfAF-YUGH_5YH75A",
-  authDomain: "autoclean360-3e0f7.firebaseapp.com",
-  projectId: "autoclean360-3e0f7",
-  storageBucket: "autoclean360-3e0f7.firebasestorage.app",
-  messagingSenderId: "194953026142",
-  appId: "1:194953026142:web:c0efe3d710ee354c7a72cf",
-  measurementId: "G-6L0FXV7MBM"
+  apiKey: "XXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  authDomain: "XXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  projectId: "XXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  storageBucket: "XXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  messagingSenderId: "XXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  appId: "XXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  measurementId: "XXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 };
 
 // Inicializar Firebase
